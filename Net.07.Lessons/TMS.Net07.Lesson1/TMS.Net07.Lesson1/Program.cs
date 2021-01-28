85c993a59bd6ac3e3dc45b11982841b81e0e6f06
KiriIl <375447360162@mail.ru>
2021-01-28 22:08:23 +0300

    repush

---
+++ Net.07.Lessons/TMS.Net07.Lesson1/TMS.Net07.Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TMS.Net07.Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!!!");
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Net07.HW/Net07.HomeWork1/TMS.Net07.HomeWork1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TMS.Net07.HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int number, endProgrammNumber = 1;
+            bool condition;
+            do
+            {
+                Console.WriteLine("Input the number");
+                str = Console.ReadLine();
+            }
+            while (!Int32.TryParse(str, out number));
+
+            Console.WriteLine($"Your number is {number}");
+
+            do
+            {
+                Console.WriteLine("Input the number to end programm");
+                str = Console.ReadLine();
+                condition = Int32.TryParse(str, out endProgrammNumber);
+                if (!condition)
+                    Console.WriteLine("Uncorrect input");
+            }
+            while (endProgrammNumber != 0 || !condition);
+        }
+    }
+}
